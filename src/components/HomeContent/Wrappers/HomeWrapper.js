932dee2caca48161e431159c314432a8d7ce65c8
Lodenn <khalednasser788@gmail.com>
2021-09-29 18:36:55 +0200

    fix fetching videos and games in homepage http request

--- src/components/HomeContent/Wrappers/HomeWrapper.js
+++ src/components/HomeContent/Wrappers/HomeWrapper.js
@@ -1,60 +1,54 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Wrapper from "../../UI/Wrapper";
 import Card from "../../UI/Card";
-import classes from "./HomeWrapper.module.scss";
 import { fetchGames, fetchVideos } from "../../../lib/api";
 import useHttp from "../../../hooks/use-http";
-import { Link } from "react-router-dom";
+import LoadingSpinner from "../../UI/LoadingSpinner";
+import classes from "./HomeWrapper.module.scss";
 
 const HomeWrapper = () => {
   const [mixedArray, setMixedArray] = useState([]);
-  const {
-    sendHttpRequest: fetchGamesRequest,
-    data: games,
-    isLoading: gamesLoading,
-  } = useHttp(fetchGames);
-  const {
-    sendHttpRequest: fetchVideosRequest,
-    data: videos,
-    isLoading: videosLoading,
-  } = useHttp(fetchVideos);
+
+  const { sendHttpRequest: fetchGamesRequest, isLoading: gamesLoading } =
+    useHttp(fetchGames);
+
+  const { sendHttpRequest: fetchVideosRequest, isLoading: videosLoading } =
+    useHttp(fetchVideos);
 
   const shuffle = (array) => {
     let currentIndex = array.length;
     let randomIndex;
-
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex],
         array[currentIndex],
       ];
     }
-
     return array;
   };
 
   useEffect(() => {
-    fetchGamesRequest();
-    fetchVideosRequest();
+    fetchGamesRequest().then((games) =>
+      fetchVideosRequest().then((videos) => {
+        const fetchedGames = games.map((game) => ({ type: "game", ...game }));
+        const fetchedVideos = videos.map((video) => ({
+          type: "video",
+          ...video,
+        }));
+        setMixedArray(shuffle([...fetchedGames, ...fetchedVideos]));
+      })
+    );
   }, [fetchGamesRequest, fetchVideosRequest]);
-
-  // if (games.length > 0 && videos.length > 0) {
-  //   const fetchedGames = games.map((game) => ({ type: "game", ...game }));
-  //   const fetchedVideos = videos.map((video) => ({
-  //     type: "video",
-  //     ...video,
-  //   }));
-  //   setMixedArray(shuffle([...fetchedGames, ...fetchedVideos]));
-  // }
 
   return (
     <Wrapper
       className={classes["home-wrapper"]}
       container={`container-grid-4x container`}
     >
+      {videosLoading && gamesLoading && <LoadingSpinner />}
       {mixedArray &&
         mixedArray.length > 0 &&
         mixedArray.map((item) => {
